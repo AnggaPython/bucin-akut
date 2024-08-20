a689864b1538dd8ed1e29db75a2a7df4071d0008
Ankara Kurnia <88472694+AnggaPython@users.noreply.github.com>
2024-08-20 13:25:11 +0700

    add file

---
+++ config.js
@@ -0,0 +1,20 @@
+const CONFIG = {
+    titleWeb: "Judul Web",
+    introTitle: 'Rosniati Molle',
+    introDesc: `Kamu seperti bintang jatuh
+Menghiasi hatiku dengan cahaya dan cinta
+Setiap senyumanmu, membuatku yakin
+Kamulah yang selama ini aku cari.`,
+    btnIntro: 'Buka Pertanyaan',
+    title: 'Apakah kamu sudah nyaman denganku? 🥰',
+    desc: 'Apakah kamu juga punya perasaan yang sama? ',
+    btnYes: 'Suka sama Angga 🥰',
+    btnNo: 'Tidak ah..',
+    question: '7 miliar orang di dunia, tapi kenapa kamu jatuh cinta sama aku?',
+    btnReply: 'Kirim ke Aku',
+    reply: 'Cinta ya cinta, kalau tidak cinta ya tetap cinta <33333333',
+    mess: 'Aku tahu kok kamu sebel sama aku karena cuek',
+    messDesc: 'tapi bukan berarti aku gak sayang sama dirimu 🥰',
+    btnAccept: 'Iya deh..',
+    messLink: 'https://wa.me/6283177731799' //link pesan kalian. Contoh: https://www.facebook.com/messages/t/100014188333536
+}
